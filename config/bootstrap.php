78a7f06197466740d1e98f86aecb6a3d13adde72
Nazario A. Ayala <nazario@niztech.com>
2018-05-15 19:57:54 -0700

    Added and configured cors plugin

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -207,6 +207,7 @@ Type::build('timestamp')
  * Plugin::load('Migrations'); //Loads a single plugin named Migrations
  *
  */
+Plugin::load('Cors', ['bootstrap' => true, 'routes' => false]);
 
 /*
  * Only try to load DebugKit in development mode
